b81f28cc5b583cfa6359edca92a7f24042831a7a
I-like_pelmani <maxtoo2017@gmail.com>
2022-01-22 19:02:44 +0200

    Add Ex_2 task

---
+++ Ex_2.py
@@ -0,0 +1,29 @@
+import sys
+import pygame
+
+class Ship:
+    def __init__(self,ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+
+        self.image = pygame.image.load('images/ppp.bmp')
+        self.rect = self.image.get_rect()
+
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        self.x = float(self.rect.x)
+
+class BG:
+    def run_game():
+        pygame.init()
+        screen = pygame.display.set_mode((800,600))
+        bg_color = (0,0,230)
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            screen.fill(bg_color)
+            pygame.display.flip()
+    run_game()
+
