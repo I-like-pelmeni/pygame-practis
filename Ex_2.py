f9339851707b2beccc139c3cb631cbe04170926e
I-like-pelmeni <maxtoo2017@gmail.com>
2022-01-29 13:19:01 +0200

    Add Ex_2 task

--- Ex_2.py
+++ Ex_2.py
@@ -2,15 +2,10 @@ import sys
 import pygame
 
 class Person:
-    def __init__(self,ai_game):
-        self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
-        self.settings = ai_game.settings
+    def __init__(self):
 
-        self.image = pygame.image.load('images/khj.bmp')
+        self.image = pygame.image.load('images/khj.png')
         self.rect = self.image.get_rect()
-
-        self.rect.midbottom = self.screen_rect.midbottom
 
         self.x = float(self.rect.x)
         self.rect.x = self.x
